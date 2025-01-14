7987a84851078e0660d92d8ed96e1cd513d18a5d
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-01-14 16:27:02 -0400

    completed assignment 2

--- M01_First_WPF_Proj/M01_First_WPF_Proj/MainWindow.xaml.cs
+++ M01_First_WPF_Proj/M01_First_WPF_Proj/MainWindow.xaml.cs
@@ -1,167 +1,213 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace M01_First_WPF_Proj
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// 3 slashes creates the comment header
-        /// </summary>
+        List<BitmapImage> hairImages = new List<BitmapImage>();
+        List<BitmapImage> eyeImages = new List<BitmapImage>();
+        List<BitmapImage> noseImages = new List<BitmapImage>();
+        List<BitmapImage> mouthImages = new List<BitmapImage>();
+
+        private int hairIndex = -1;
+        private int eyeIndex = -1;
+        private int noseIndex = -1;
+        private int mouthIndex = -1;
+
+        private bool updateHair = false;
+        private bool updateEyes = false;
+        private bool updateNose = false;
+        private bool updateMouth = false;
+
+        Random random = new Random();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Call the load method to populate the lists
+            LoadCategoryImages("../../images/hair", hairImages);
+            LoadCategoryImages("../../images/eyes", eyeImages);
+            LoadCategoryImages("../../images/nose", noseImages);
+            LoadCategoryImages("../../images/mouth", mouthImages);
         }
 
-
-        /// <summary>
-        /// Draw a line on the canvas, using the supplied points.
-        /// </summary>
-        /// <param name="xLoc1">X1</param>
-        /// <param name="yLoc1">Y1</param>
-        /// <param name="xLoc2">X2</param>
-        /// <param name="yLoc2">Y2</param>
-        public void MyLineMethod(int xLoc1, int yLoc1, int xLoc2, int yLoc2)
+        // Method to populate the lists with BM images based on whatever is in the folders.
+        private static void LoadCategoryImages(string folderPath, List<BitmapImage> imageList)
         {
-            // Add line to Grid
-            Line myLine = new Line();
-            myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
-            myLine.X1 = xLoc1;
-            myLine.Y1 = yLoc1;
-            myLine.X2 = xLoc2 + 50;
-            myLine.Y2 = yLoc2 + 50;
-            myLine.HorizontalAlignment = HorizontalAlignment.Left;
-            myLine.VerticalAlignment = VerticalAlignment.Center;
-            myLine.StrokeThickness = 2;
-            myCanvas.Children.Add(myLine);
-            //myGrid.Children.Add(myLine);
-
+            try
+            {
+                // Populate imageFiles with all of the files with specified file extensions in folder path
+                string[] imageFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                                                .Where(file => file.EndsWith(".png") ||
+                                                               file.EndsWith(".jpg"))
+                                                .ToArray();
+                
+                // For each image file, create a bitmap image and add it to the list that was passed
+                foreach (string file in imageFiles)
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(file, UriKind.Relative);
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                    imageList.Add(bitmap);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading images from {folderPath}: {ex.Message}");
+            }
         }
 
-        /// <summary>
-        /// Random Number generator for line positions.
-        /// </summary>
-        Random random = new Random();
-
-        /// <summary>
-        /// Event Handler for button.  Not renamed, so lots of work to 
-        /// name it properly now (but not impossible).
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button_Click(object sender, RoutedEventArgs e)
+        public void MyImageMethod()
         {
-            int rx1 = random.Next(10, 200);
-            int ry1 = random.Next(10, 200);
-            int rx2 = random.Next(10, 200);
-            int ry2 = random.Next(10, 200);
-            MyLineMethod(rx1, ry1, rx2, ry2);
+            // Create new image objects
+            Image imgHair = new Image();
+            Image imgEyes = new Image();
+            Image imgNose = new Image();
+            Image imgMouth = new Image();
+
+            // Draw the hair on the canvas
+            if (hairIndex >= 0 && updateHair)
+            {
+                imgHair.Source = hairImages[hairIndex];
+                Canvas.SetLeft(imgHair, 0);
+                Canvas.SetTop(imgHair, 0); // Images are 250H so incrementing this by 250 aligns them properly
+                myCanvas.Children.Add(imgHair);
+
+                updateHair = false;
+            }
+
+            //Draw the eyes on the canvas
+            if (eyeIndex >= 0 && updateEyes)
+            {
+                imgEyes.Source = eyeImages[eyeIndex];
+                Canvas.SetLeft(imgEyes, 0);
+                Canvas.SetTop(imgEyes, 250);
+                myCanvas.Children.Add(imgEyes);
+
+                updateEyes = false;
+            }
+
+            // Draw the nose on the canvas
+            if (noseIndex >= 0 && updateNose)
+            {
+                imgNose.Source = noseImages[noseIndex];
+                Canvas.SetLeft(imgNose, 0);
+                Canvas.SetTop(imgNose, 500);
+                myCanvas.Children.Add(imgNose);
+
+                updateNose = false;
+            }
+
+            // Draw the mouth on the canvas
+            if (mouthIndex >= 0 && updateMouth)
+            {
+                imgMouth.Source = mouthImages[mouthIndex];
+                Canvas.SetLeft(imgMouth, 0);
+                Canvas.SetTop(imgMouth, 750);
+                myCanvas.Children.Add(imgMouth);
+
+                updateMouth = false;
+            }
         }
 
-        /// <summary>
-        /// Just drag a checkbox onto the grid and double-click it to
-        /// create this event handler.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        private void OnButtonClicked(object sender, RoutedEventArgs e)
         {
-            int rx1 = random.Next(10, 200);
-            int ry1 = random.Next(10, 200);
-            int rx2 = random.Next(10, 200);
-            int ry2 = random.Next(10, 200);
-            MyLineMethod(rx1, ry1, rx2, ry2);
+            // Get the button that was pressed
+            if (sender is Button clickedButton)
+            {
+                HandleButtonPress(clickedButton);
+            }
         }
 
-
-
-
-        /// <summary>
-        /// Load the face image file from local directory.  
-        /// 
-        /// To make a transparent GIF, use Gimp2 and delete
-        /// background layer using fuzzy select->delete.
-        /// 
-        /// Images (images dir) needs to be in the "M01_First_WPF_Proj\bin\Debug" directory
-        /// </summary>
-        BitmapImage BMimg = new BitmapImage(new Uri("../../images/layerA.gif", UriKind.Relative));
-
-
-        /// <summary>
-        /// Draw an image on thre canvas at the supplied x,y location.
-        /// </summary>
-        /// <param name="xLoc1"></param>
-        /// <param name="yLoc1"></param>
-        public void MyImageMethod(int xLoc1, int yLoc1)
+        // Method to update the respective index based on what button was pressed (should clean up).
+        private void HandleButtonPress(Button button)
         {
-            // may need to copy /images/*.png into /Debug folder
-            // Need to create a new image every time we place a copy on the canvas
-            Image img = new Image();
+            switch (button.Name)
+            {
+                case "hair_next":
+                    hairIndex = IncrementChoice(hairIndex, hairImages);
+                    updateHair = true;
+                    break;
+                case "hair_prev":
+                    hairIndex = DecrementChoice(hairIndex, hairImages);
+                    updateHair = true;
+                    break;
+                case "eyes_next":
+                    eyeIndex = IncrementChoice(eyeIndex, eyeImages);
+                    updateEyes = true;
+                    break;
+                case "eyes_prev":
+                    eyeIndex = DecrementChoice(eyeIndex, eyeImages);
+                    updateEyes = true;
+                    break;
+                case "nose_next":
+                    noseIndex = IncrementChoice(noseIndex, noseImages);
+                    updateNose = true;
+                    break;
+                case "nose_prev":
+                    noseIndex = DecrementChoice(noseIndex, noseImages);
+                    updateNose = true;
+                    break;
+                case "mouth_next":
+                    mouthIndex = IncrementChoice(mouthIndex, mouthImages);
+                    updateMouth = true;
+                    break;
+                case "mouth_prev":
+                    mouthIndex = DecrementChoice(mouthIndex, mouthImages);
+                    updateMouth = true;
+                    break;
+                case "randomize":
+                    RandomizeChoice();
+                    break;
+                default:
+                    break;
+            }
 
-            // Minimum image attributes
-            img.Source = BMimg;
-            img.Width = BMimg.Width;  // exception if images not copied down
-            img.Height = BMimg.Height;
-
-            // Attach to canvas in position
-            Canvas.SetLeft(img, xLoc1);
-            Canvas.SetTop(img, yLoc1);
-            myCanvas.Children.Add(img);
-
+            MyImageMethod();
         }
 
-        /// <summary>
-        /// Button to add the image, which happens to be a face
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Add_Face_Click(object sender, RoutedEventArgs e)
+        // Method to incremenet indexes for their respective lists
+        private static int IncrementChoice(int index, List<BitmapImage> imageList)
         {
-            int rx1 = random.Next(10, 200);
-            int ry1 = random.Next(10, 200);
-            MyImageMethod(rx1, ry1);
+            if (index < 0 || index >= imageList.Count - 1)
+            {
+                return 0;
+            }
 
+            return index + 1;
         }
 
-        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        // Method to decremenet indexes for their respective lists
+        private static int DecrementChoice(int index, List<BitmapImage> imageList)
         {
-            Console.WriteLine("Button was pressed...");
+            if (index <= 0) {
+
+                return imageList.Count - 1;
+            }
+
+            return index - 1;
         }
 
-        private void comboTest_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        // Method to randomize the indexes
+        private void RandomizeChoice()
         {
-            //MessageBox.Show(e.ToString());
-            //MessageBox.Show(comboTest.SelectedItem.ToString());
-            Console.WriteLine("Combo=" + comboTest.SelectedItem.ToString());
-        }
+            hairIndex = random.Next(0, hairImages.Count);
+            eyeIndex = random.Next(0, eyeImages.Count);
+            noseIndex = random.Next(0, noseImages.Count);
+            mouthIndex = random.Next(0, mouthImages.Count);
 
-        private void sliderTest_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            //MessageBox.Show(e.ToString());
-            //MessageBox.Show(sliderTest.Value.ToString());
-            Console.WriteLine("Slider=" + sliderTest.Value.ToString());
-            Console.WriteLine("Slider e=" + e.ToString());
-
-        }
-
-        private void combo_02_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            updateHair = updateEyes = updateNose = updateMouth = true;
         }
     }
 }
