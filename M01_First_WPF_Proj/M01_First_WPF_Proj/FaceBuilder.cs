b33383c3e57e62f9e4d637046f000f42f2bc3a6b
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-01-22 12:07:11 -0400

    refactored codebase and separated classes

---
+++ M01_First_WPF_Proj/M01_First_WPF_Proj/FaceBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace M01_First_WPF_Proj
+{
+    public  static class FaceBuilder
+    {
+
+        public  static void hairNext()
+        {
+            
+        } 
+
+    }
+}
--- M01_First_WPF_Proj/M01_First_WPF_Proj/MainWindow.xaml.cs
+++ M01_First_WPF_Proj/M01_First_WPF_Proj/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
 namespace M01_First_WPF_Proj
@@ -11,170 +12,56 @@ namespace M01_First_WPF_Proj
 
     public partial class MainWindow : Window
     {
-        List<BitmapImage> hairImages = new List<BitmapImage>();
-        List<BitmapImage> eyeImages = new List<BitmapImage>();
-        List<BitmapImage> noseImages = new List<BitmapImage>();
-        List<BitmapImage> mouthImages = new List<BitmapImage>();
 
-        private int hairIndex = -1;
-        private int eyeIndex = -1;
-        private int noseIndex = -1;
-        private int mouthIndex = -1;
+        private ImageManager imageManager = new ImageManager();
 
-        private bool updateHair = false;
-        private bool updateEyes = false;
-        private bool updateNose = false;
-        private bool updateMouth = false;
-
-        Random random = new Random();
-
+        private CommandHandler cmdHairNext = new CommandHandler(FaceBuilder.hairNext, true);
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initialize ImageManager
+            imageManager.LoadImages("../../images/hair", "../../images/eyes", "../../images/nose", "../../images/mouth");
+
+            DataContext = new
+            {
+                nextHairCMD = cmdHairNext
+            };
+
+            InputBindings.Add(new KeyBinding(cmdHairNext, new KeyGesture(Key.R, ModifierKeys.Control)));
             
-            // Call the load method to populate the lists
-            LoadCategoryImages("../../images/hair", hairImages);
-            LoadCategoryImages("../../images/eyes", eyeImages);
-            LoadCategoryImages("../../images/nose", noseImages);
-            LoadCategoryImages("../../images/mouth", mouthImages);
         }
 
-        // Method to populate the lists with BM images based on whatever is in the folders.
-        private static void LoadCategoryImages(string folderPath, List<BitmapImage> imageList)
+        private void UpdateCanvas()
         {
-            try
-            {
-                // Populate imageFiles with all of the files with specified file extensions in folder path
-                string[] imageFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
-                                                .Where(file => file.EndsWith(".png") ||
-                                                               file.EndsWith(".jpg"))
-                                                .ToArray();
-                
-                // For each image file, create a bitmap image and add it to the list that was passed
-                foreach (string file in imageFiles)
-                {
-                    BitmapImage bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.UriSource = new Uri(file, UriKind.Relative);
-                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmap.EndInit();
-                    imageList.Add(bitmap);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error loading images from {folderPath}: {ex.Message}");
-            }
+            myCanvas.Children.Clear();
+
+            DrawImage(imageManager.GetHairImage(), 0, 0);
+            DrawImage(imageManager.GetEyeImage(), 0, 250);
+            DrawImage(imageManager.GetNoseImage(), 0, 500);
+            DrawImage(imageManager.GetMouthImage(), 0, 750);
+
         }
 
-        public void MyImageMethod()
+        private void DrawImage(BitmapImage image, double left, double top)
         {
-            // Create new image objects
-            Image imgHair = new Image();
-            Image imgEyes = new Image();
-            Image imgNose = new Image();
-            Image imgMouth = new Image();
-
-            // Draw the hair on the canvas
-            if (hairIndex >= 0 && updateHair)
+            if (image != null)
             {
-                imgHair.Source = hairImages[hairIndex];
-                Canvas.SetLeft(imgHair, 0);
-                Canvas.SetTop(imgHair, 0); // Images are 250H so incrementing this by 250 aligns them properly
-                myCanvas.Children.Add(imgHair);
-
-                updateHair = false;
-            }
-
-            //Draw the eyes on the canvas
-            if (eyeIndex >= 0 && updateEyes)
-            {
-                imgEyes.Source = eyeImages[eyeIndex];
-                Canvas.SetLeft(imgEyes, 0);
-                Canvas.SetTop(imgEyes, 250);
-                myCanvas.Children.Add(imgEyes);
-
-                updateEyes = false;
-            }
-
-            // Draw the nose on the canvas
-            if (noseIndex >= 0 && updateNose)
-            {
-                imgNose.Source = noseImages[noseIndex];
-                Canvas.SetLeft(imgNose, 0);
-                Canvas.SetTop(imgNose, 500);
-                myCanvas.Children.Add(imgNose);
-
-                updateNose = false;
-            }
-
-            // Draw the mouth on the canvas
-            if (mouthIndex >= 0 && updateMouth)
-            {
-                imgMouth.Source = mouthImages[mouthIndex];
-                Canvas.SetLeft(imgMouth, 0);
-                Canvas.SetTop(imgMouth, 750);
-                myCanvas.Children.Add(imgMouth);
-
-                updateMouth = false;
+                Image img = new Image { Source = image };
+                Canvas.SetLeft(img, left);
+                Canvas.SetTop(img, top);
+                myCanvas.Children.Add(img);
             }
         }
 
         private void OnButtonClicked(object sender, RoutedEventArgs e)
         {
-            // Get the button that was pressed
-            if (sender is Button clickedButton)
-            {
-                HandleButtonPress(clickedButton);
-            }
-        }
-
-        // Method to update the respective index based on what button was pressed (should clean up).
-        private void HandleButtonPress(Button button)
-        {
-            switch (button.Name)
-            {
-                case "hair_next":
-                    hairIndex = IncrementChoice(hairIndex, hairImages);
-                    updateHair = true;
-                    break;
-                case "hair_prev":
-                    hairIndex = DecrementChoice(hairIndex, hairImages);
-                    updateHair = true;
-                    break;
-                case "eyes_next":
-                    eyeIndex = IncrementChoice(eyeIndex, eyeImages);
-                    updateEyes = true;
-                    break;
-                case "eyes_prev":
-                    eyeIndex = DecrementChoice(eyeIndex, eyeImages);
-                    updateEyes = true;
-                    break;
-                case "nose_next":
-                    noseIndex = IncrementChoice(noseIndex, noseImages);
-                    updateNose = true;
-                    break;
-                case "nose_prev":
-                    noseIndex = DecrementChoice(noseIndex, noseImages);
-                    updateNose = true;
-                    break;
-                case "mouth_next":
-                    mouthIndex = IncrementChoice(mouthIndex, mouthImages);
-                    updateMouth = true;
-                    break;
-                case "mouth_prev":
-                    mouthIndex = DecrementChoice(mouthIndex, mouthImages);
-                    updateMouth = true;
-                    break;
-                case "randomize":
-                    RandomizeChoice();
-                    break;
-                default:
-                    break;
-            }
-
-            MyImageMethod();
+            //// Get the button that was pressed
+            //if (sender is Button clickedButton)
+            //{
+            //    HandleButtonPress(clickedButton);
+            //}
         }
 
         // Method to incremenet indexes for their respective lists
@@ -200,14 +87,9 @@ namespace M01_First_WPF_Proj
         }
 
         // Method to randomize the indexes
-        private void RandomizeChoice()
+        private void RandomizeChoice(object sender, RoutedEventArgs e)
         {
-            hairIndex = random.Next(0, hairImages.Count);
-            eyeIndex = random.Next(0, eyeImages.Count);
-            noseIndex = random.Next(0, noseImages.Count);
-            mouthIndex = random.Next(0, mouthImages.Count);
-
-            updateHair = updateEyes = updateNose = updateMouth = true;
+            imageManager.RandomizeIndexes();
         }
     }
 }
