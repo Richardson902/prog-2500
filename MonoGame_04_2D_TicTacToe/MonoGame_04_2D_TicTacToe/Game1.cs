a8c0f4bc21214f4991d7fc310901c8f67e105ee0
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-03-25 14:19:24 -0300

    init commit

---
+++ MonoGame_04_2D_TicTacToe/MonoGame_04_2D_TicTacToe/Game1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame_04_2D_TicTacToe;
+
+
+/**
+* 3D: Place an Image on the Screen using XNA:
+* ===========================================
+* This sample:
+*      - Uses MonoGame programming...used to be xBox/XNA
+*      - Shows how 3D mesh shapes are placed on the screen
+*      - Too functional, should use more OOP
+*      - myModel class isn't bad, but grid should be a class with it's own draw()
+*      - note enumerations used to show meaning in variables
+*      
+*      
+**/
+
+public class Game1 : Game
+{
+    private Grid grid;
+    public static GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+
+    // Set the position of the camera in world space, for our view matrix.
+    Vector3 cameraPosition = new Vector3(0.0f, 350.0f, 350.0f);
+
+
+    // List of models to draw
+    System.Collections.ArrayList Model_list = new System.Collections.ArrayList();
+
+
+    // Used to detect the B button being pressed
+    ButtonState lastUpdateState;
+    ButtonState thisUpdateState;
+
+    // The aspect ratio determines how to scale 3d to 2d projection.
+    float aspectRatio;
+
+
+    // each and any spot my be selected
+    private Boolean[,] selection = new Boolean[3, 3];
+
+    // current "cursor" in 2D
+    private int[] current = new int[2];
+
+    Boolean lastTimeWasNotSpace = false;
+    Boolean lastTimeWasNotUp = false;
+    Boolean lastTimeWasNotRight = false;
+
+
+    private myModel[] m = new myModel[4];
+
+
+    protected override void LoadContent()
+    {
+
+        Debug.WriteLine("BasicCameraSample LoadContent");
+
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        grid = new Grid();
+
+        // Flyweight pattern: create one of each mesh, and reuse
+        // these 4 meshes in the grid.  
+        // load each mesh one at a time, 4 types of mesh objects
+        // All mesh objects displayed, will render from one of these 4
+        myModel.setupGraphics(_graphics);
+        m[(int)Grid.GridVal.Center] = new myModel(Content.Load<Model>("Models\\Z"), Vector3.Zero, Vector3.Zero, Color.Yellow);
+        m[(int)Grid.GridVal.X] = new myModel(Content.Load<Model>("Models\\X"), Vector3.Zero, Vector3.Zero, Color.Yellow);
+        m[(int)Grid.GridVal.O] = new myModel(Content.Load<Model>("Models\\O"), Vector3.Zero, Vector3.Zero, Color.Yellow);
+        m[(int)Grid.GridVal.Dot] = new myModel(Content.Load<Model>("Models\\dot"), Vector3.Zero, Vector3.Zero, Color.Yellow);
+
+        aspectRatio = (float)_graphics.GraphicsDevice.Viewport.Width /
+                        (float)_graphics.GraphicsDevice.Viewport.Height;
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+
+
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        //Get Keyboard Keystroke
+        KeyboardState k = Keyboard.GetState();
+
+        // Act on the keystroke
+        if (k.IsKeyDown(Keys.E))
+            this.Exit();                                      // E = Exit Game
+
+
+        // cameraPosition
+        if (k.IsKeyDown(Keys.D1))
+        {
+            cameraPosition = new Vector3(0.0f, 350.0f, 350.0f);
+        }
+        if (k.IsKeyDown(Keys.D2))
+        {
+            cameraPosition = new Vector3(50.0f, 450.0f, 450.0f);
+        }
+        if (k.IsKeyDown(Keys.D3))
+        {
+            cameraPosition = new Vector3(100.0f, 250.0f, 250.0f);
+        }
+        if (k.IsKeyDown(Keys.D4))
+        {
+            cameraPosition = new Vector3(0.0f, -350.0f, 250.0f);
+        }
+
+
+
+
+        if ((k.IsKeyDown(Keys.Up)) && lastTimeWasNotUp)
+        {
+            grid.CurrentPosition[1] = (grid.CurrentPosition[1] + 1) % 3;
+        }
+
+        if ((k.IsKeyDown(Keys.Right)) && lastTimeWasNotRight)
+        {
+            grid.CurrentPosition[0] = (grid.CurrentPosition[0] + 1) % 3;
+        }
+
+        if (k.IsKeyDown(Keys.Space) && lastTimeWasNotSpace)
+        {
+            grid.ToggleSelection(grid.CurrentPosition[0], grid.CurrentPosition[1]);
+            grid.UpdateGridValue(grid.CurrentPosition[0], grid.CurrentPosition[1]);
+        }
+
+        lastTimeWasNotUp = !k.IsKeyDown(Keys.Up);
+        lastTimeWasNotRight = !k.IsKeyDown(Keys.Right);
+        lastTimeWasNotSpace = !k.IsKeyDown(Keys.Space);
+
+        grid.WhoWon = grid.GetWhoWon();
+
+        base.Update(gameTime);
+    }
+
+
+
+    Vector3 cameraTarget = Vector3.Zero;
+    Vector3 cameraUpDirection = Vector3.Up;
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        grid.Draw(m, cameraPosition, aspectRatio, cameraTarget, cameraUpDirection);
+
+        if (grid.WhoWon != Grid.GridVal.Dot)
+        {
+            Debug.WriteLine("Player " + grid.WhoWon + " won!");
+        }
+
+        base.Draw(gameTime);
+    }
+}
+
---
+++ MonoGame_04_2D_TicTacToe/MonoGame_04_2D_TicTacToe/Grid.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame_04_2D_TicTacToe
+{
+    class Grid
+    {
+        public enum GridVal { X = 0, O = 1, Center = 2, Dot = 3 }
+
+        private GridVal[,] gridValues = new GridVal[3, 3];
+        private bool[,] selection = new bool[3, 3];
+
+        public int[] CurrentPosition { get; set; } = new int[2];
+        public GridVal WhoWon { get; set; }
+        private GridVal[] turn = { GridVal.X, GridVal.O };
+
+        public Grid()
+        {
+            SetAllGridPos(GridVal.Dot);
+            SetAllNotSelected();
+        }
+
+        public void SetGridPos(GridVal v, int x, int y)
+        {
+            gridValues[x, y] = v;
+        }
+
+        public void SetAllGridPos(GridVal v)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    SetGridPos(v, i, j);
+                }
+            }
+        }
+
+        public void SetAllNotSelected()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    selection[i, j] = false;
+                }
+            }
+        }
+
+        public void ToggleSelection(int x, int y)
+        {
+            selection[x, y] = !selection[x, y];
+        }
+
+        public void UpdateGridValue(int x, int y)
+        {
+            int eVal = (int)gridValues[x, y];
+            int max_eVal = Enum.GetNames(typeof(GridVal)).Length;
+            eVal = (eVal + 1) % max_eVal;
+            gridValues[x, y] = (GridVal)eVal;
+        }
+
+        public GridVal GetGridValue(int x, int y)
+        {
+            return gridValues[x, y];
+        }
+
+        public bool IsSelected(int x, int y)
+        {
+            return selection[x, y];
+        }
+
+        public GridVal GetWhoWon()
+        {
+            foreach (var player in turn)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    // Horizontal
+                    if ((gridValues[i, 0] == player) && (gridValues[i, 1] == player) && (gridValues[i, 2] == player))
+                    {
+                        return player;
+                    }
+
+                    // Vertical
+                    if ((gridValues[0, i] == player) && (gridValues[1, i] == player) && (gridValues[2, i] == player))
+                    {
+                        return player;
+                    }
+                }
+
+                // Diagonals
+                if ((gridValues[0, 0] == player) && (gridValues[1, 1] == player) && (gridValues[2, 2] == player))
+                {
+                    return player;
+                }
+
+                if ((gridValues[2, 0] == player) && (gridValues[1, 1] == player) && (gridValues[0, 2] == player))
+                {
+                    return player;
+                }
+            }
+
+            return GridVal.Dot;
+        }
+
+        public void Draw(myModel[] models, Vector3 cameraPosition, float aspectRatio, Vector3 cameraTarget, Vector3 cameraUpDirection)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool won = ((gridValues[i, j] != GridVal.Dot) && (WhoWon == gridValues[i, j]));
+
+                    Vector3 location = new Vector3(
+                        i * 100 - 50.0f,
+                        j * 100 - 50.0f,
+                        0.0f
+                    );
+
+                    myModel here = models[(int)gridValues[i, j]];
+                    here.pos = location;
+
+                    here.color = Color.PaleGreen;
+                    if (gridValues[i, j] == GridVal.X) { here.color = Color.BlueViolet; }
+                    if (gridValues[i, j] == GridVal.O) { here.color = Color.DarkOrange; }
+
+                    if ((i == CurrentPosition[0]) && (j == CurrentPosition[1]))
+                    {
+                        here.color = Color.Red;
+                    }
+
+                    here.draw(cameraPosition, aspectRatio, cameraTarget, cameraUpDirection, won);
+                }
+            }
+        }
+
+
+    }
+}
